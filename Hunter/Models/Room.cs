b65535151898f7c13b060e27b7cd94c3f3837cb8
轩宇 林 <lxymhxy@hotmail.com>
2017-07-09 20:52:54 +0800

    alpha

---
+++ Hunter/Models/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter.Models
+{
+    class Room
+    {
+    }
+    public class MissionList
+    {
+        public string name { get; set; }
+    }
+
+    public class ListManager
+    {
+
+        public static List<MissionList> getInstance()
+        {
+            var lists = new List<MissionList> { };
+            lists.Add(new MissionList { name = "111111111" });
+            return lists;
+        }
+    }
+}
