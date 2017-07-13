4d135bdcdf78f5bacff941c91dd203efc692eed8
Wu Zebin <wuzb19951013@gmail.com>
2017-07-13 11:43:52 +0800

    测试后退按键

--- Hunter/App.xaml.cs
+++ Hunter/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -56,6 +57,9 @@ namespace Hunter
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
+                //设置页面跳转后退按钮
+                rootFrame.Navigated += RootFrame_Navigated;
+
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: Load state from previously suspended application
@@ -74,8 +78,38 @@ namespace Hunter
                     // parameter
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
+
+                //注册 backbutton 事件
+                SystemNavigationManager.GetForCurrentView().BackRequested += App_BackRequested;
                 // Ensure the current window is active
                 Window.Current.Activate();
+            }
+        }
+
+        // 每次导航事件发生时，更新返回键的可见信息
+        private void RootFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                ((Frame)sender).CanGoBack ?
+                AppViewBackButtonVisibility.Visible :
+                AppViewBackButtonVisibility.Collapsed;
+        }
+
+        //titleBar 返回事件
+        private void App_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                return;
+            }
+
+            //e.Handled 该值指示应用程序是否执行了请求的返回导航功能
+            if (rootFrame.CanGoBack && e.Handled == false)
+            {
+                e.Handled = true;
+                rootFrame.GoBack();
             }
         }
 
