18050d83be4f68cde9a064bd316883ee9680329a
轩宇 林 <lxymhxy@hotmail.com>
2017-07-14 11:07:06 +0800

    标题栏返回功能

--- Hunter/Room/RoomPage.xaml.cs
+++ Hunter/Room/RoomPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,8 +33,9 @@ namespace Hunter.Room
         }
         public RoomPage()
         {
-            MyList = ListManager.getInstance(); 
+            MyList = ListManager.getInstance();
             this.InitializeComponent();
+            
         }
 
         private void headicon_Click(object sender, RoutedEventArgs e)
