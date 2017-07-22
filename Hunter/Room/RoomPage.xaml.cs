36650660b137223e2eb9b49e1abc7aacff7d174f
轩宇 林 <lxymhxy@hotmail.com>
2017-07-22 16:53:25 +0800

    列表筛选

--- Hunter/Room/RoomPage.xaml.cs
+++ Hunter/Room/RoomPage.xaml.cs
@@ -59,6 +59,9 @@ namespace Hunter.Room
 
         private async void refreshbutton_ClickAsync(object sender, RoutedEventArgs e)
         {
+            selectByBackgroundComboBox.SelectedIndex = 0;
+            selectByDifficultyComboBox.SelectedIndex = 0;
+            selectByThemeComboBox.SelectedIndex = 0;
             using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
             {
                 TimeSpan ts = new TimeSpan(15000000);
@@ -125,19 +128,219 @@ namespace Hunter.Room
             Frame.Navigate(typeof(Settings.Settings));
         }
 
-        private void selectByThemeComboBox_DropDownClosed(object sender, object e)
+        private async void selectByThemeComboBox_DropDownClosedAsync(object sender, object e)
         {
 
+            if (theme0.IsSelected)
+            {
+                refreshbutton_ClickAsync(sender, (RoutedEventArgs)e);
+                return;
+            }
+            else
+            {
+
+                using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+                {
+                    TimeSpan ts = new TimeSpan(15000000);
+                    client.Timeout = ts;
+                    try
+                    {
+                        var kvp = new List<KeyValuePair<string, string>>
+                    {
+
+                        new KeyValuePair<string,string>("action", "getmission"),
+                    };
+                        var response = await client.PostAsync("http://qwq.itbears.club/hunter.php", new FormUrlEncodedContent(kvp));
+
+                        string result = await response.Content.ReadAsStringAsync();
+                        if (result == "false")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            var s = new DataContractJsonSerializer(typeof(RootObject[]));
+                            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+                            RootObject[] data = (RootObject[])s.ReadObject(ms);
+                            int i = 0;
+                            MissionList.Clear();
+                            while (i < data.Length)
+                            {
+                                if (data[i].Classes == selectByThemeComboBox.SelectionBoxItem.ToString())
+                                {
+                                    if (selectByDifficultyComboBox.SelectedIndex == 0 && selectByDifficultyComboBox.SelectedIndex == 0)
+                                    {
+                                        MissionList.Add(data[i]);
+                                    }
+                                    else
+                                    {
+                                        if (data[i].Addr == selectByBackgroundComboBox.SelectionBoxItem.ToString())
+                                        {
+                                            MissionList.Add(data[i]);
+                                        }
+
+                                    }
+                                }
+                               
+                                i++;
+                            }
+                        }
+
+                    }
+                    catch
+                    {
+                        var msgDialog = new Windows.UI.Popups.MessageDialog("网络可能开小差了，请稍后再试") { Title = "刷新失败" };
+                        msgDialog.Commands.Add(new Windows.UI.Popups.UICommand("确定", uiCommand => { }));
+                        await msgDialog.ShowAsync();
+
+                    }
+                    finally
+                    {
+
+                    }
+
+
+                }
+
+            }
         }
 
-        private void selectByBackgroundComboBox_DropDownClosed(object sender, object e)
+        private async void selectByBackgroundComboBox_DropDownClosedAsync(object sender, object e)
         {
+            if (background0.IsSelected)
+            {
+                refreshbutton_ClickAsync(sender, (RoutedEventArgs)e);
+                return;
+            }
+            else
+            {
+                using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+                {
+                    TimeSpan ts = new TimeSpan(15000000);
+                    client.Timeout = ts;
+                    try
+                    {
+                        var kvp = new List<KeyValuePair<string, string>>
+                    {
 
+                        new KeyValuePair<string,string>("action", "getmission"),
+                    };
+                        var response = await client.PostAsync("http://qwq.itbears.club/hunter.php", new FormUrlEncodedContent(kvp));
+
+                        string result = await response.Content.ReadAsStringAsync();
+                        if (result == "false")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            var s = new DataContractJsonSerializer(typeof(RootObject[]));
+                            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+                            RootObject[] data = (RootObject[])s.ReadObject(ms);
+                            int i = 0;
+                            MissionList.Clear();
+                            while (i < data.Length)
+                            {
+                                if (data[i].Addr == selectByBackgroundComboBox.SelectionBoxItem.ToString())
+                                {
+                                    if(selectByThemeComboBox.SelectedIndex==0&&selectByDifficultyComboBox.SelectedIndex==0)
+                                    {
+                                        MissionList.Add(data[i]);
+                                    }
+                                    else
+                                    {
+                                        if (data[i].Classes == selectByThemeComboBox.SelectionBoxItem.ToString())
+                                        {
+                                            MissionList.Add(data[i]);
+                                        }
+
+                                    }
+                                }
+                                i++;
+                            }
+                        }
+
+                    }
+                    catch
+                    {
+                        var msgDialog = new Windows.UI.Popups.MessageDialog("网络可能开小差了，请稍后再试") { Title = "刷新失败" };
+                        msgDialog.Commands.Add(new Windows.UI.Popups.UICommand("确定", uiCommand => { }));
+                        await msgDialog.ShowAsync();
+
+                    }
+                    finally
+                    {
+
+                    }
+
+
+                }
+               
+            }
         }
 
-        private void selectByDifficultyComboBox_DropDownClosed(object sender, object e)
+        private async void selectByDifficultyComboBox_DropDownClosedAsync(object sender, object e)
         {
+            if (level0.IsSelected)
+            {
+                refreshbutton_ClickAsync(sender, (RoutedEventArgs)e);
+                return;
+            }
+            else
+            {
 
+                using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+                {
+                    TimeSpan ts = new TimeSpan(15000000);
+                    client.Timeout = ts;
+                    try
+                    {
+                        var kvp = new List<KeyValuePair<string, string>>
+                    {
+
+                        new KeyValuePair<string,string>("action", "getmission"),
+                    };
+                        var response = await client.PostAsync("http://qwq.itbears.club/hunter.php", new FormUrlEncodedContent(kvp));
+
+                        string result = await response.Content.ReadAsStringAsync();
+                        if (result == "false")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            var s = new DataContractJsonSerializer(typeof(RootObject[]));
+                            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+                            RootObject[] data = (RootObject[])s.ReadObject(ms);
+                            int i = 0;
+                            MissionList.Clear();
+                            while (i < data.Length)
+                            {
+                                /*if (data[i].Classes == selectByDifficultyComboBox.SelectionBoxItem.ToString())
+                                {
+                                    MissionList.Add(data[i]);
+                                }*/
+                                i++;
+                            }
+                        }
+
+                    }
+                    catch
+                    {
+                        var msgDialog = new Windows.UI.Popups.MessageDialog("网络可能开小差了，请稍后再试") { Title = "刷新失败" };
+                        msgDialog.Commands.Add(new Windows.UI.Popups.UICommand("确定", uiCommand => { }));
+                        await msgDialog.ShowAsync();
+
+                    }
+                    finally
+                    {
+
+                    }
+
+
+                }
+
+            }
         }
     }
 }
